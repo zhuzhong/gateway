31b590664f9474400a06368e9d836b57e902a06a
zhuzhong <zhuzhong@aladingbank.com>
2017-04-19 10:14:08 +0800

    项目重构

---
+++ gateway-core/src/main/java/com/aldb/gateway/util/OpenApiResponseUtils.java
@@ -0,0 +1,55 @@
+package com.aldb.gateway.util;
+
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.aldb.gateway.common.OpenApiHttpRequestBean;
+
+public class OpenApiResponseUtils {
+
+
+    
+    private static Log logger = LogFactory.getLog(OpenApiResponseUtils.class);
+    
+    
+    public static final String CONTENT_TYPE_KEY = "content-type";
+    public static final String CONTENT_TYPE_XML = "application/xml";
+    public static final String CONTENT_TYPE_JSON = "application/json";
+    public static final String HEADER_HOST_KEY = "host";
+    public static final String HEADER_SERVER_KEY = "server";
+   // public static Map<String, HttpServletResponse> sessionMap = new HashMap<String, HttpServletResponse>();
+
+    public static void writeRsp(HttpServletResponse response, OpenApiHttpRequestBean requestBean) {
+        setResponseHeader(response, requestBean.getReqHeader());
+        try {
+            PrintWriter writer = response.getWriter();
+            writer.print(requestBean.getPrintStr());
+            writer.flush();
+            writer.close();
+        } catch (Exception e) {
+            logger.error("Write body to response error, " + e.getMessage());
+        } 
+        /*finally {
+            sessionMap.remove(requestBean.getReqId());
+        }*/
+    }
+
+    private static void setResponseHeader(HttpServletResponse response, Map<String, String> httpHeader) {
+        Iterator<Entry<String, String>> entries = httpHeader.entrySet().iterator();
+        while (entries.hasNext()) {
+            Entry<String, String> entry = entries.next();
+            if (entry.getKey().equals(CONTENT_TYPE_KEY) || entry.getKey().equals(HEADER_HOST_KEY)) {
+                response.addHeader(entry.getKey(), entry.getValue());
+            }
+        }
+        response.setHeader(HEADER_SERVER_KEY, null);
+    }
+}
---
+++ gateway-service/src/main/java/com/aldb/gateway/service/support/DefaultIdServiceImpl.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.aldb.gateway.service.support;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.springframework.stereotype.Service;
+
+import com.aldb.gateway.service.IdService;
+
+/**
+ * @author Administrator
+ *
+ */
+@Service
+public class DefaultIdServiceImpl implements IdService {
+
+    private AtomicInteger ai = new AtomicInteger(1);
+
+    @Override
+    public String genInnerRequestId() {
+        return String.valueOf(ai.getAndIncrement());
+    }
+    
+    
+
+}
